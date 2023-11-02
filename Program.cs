875192d94c432a487ff18afb92bfb765dd273835
Vanessa Vieira <vaninha049@hotmail.com>
2023-11-01 22:29:00 -0300

    Segundo commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
-Nokia n = new Nokia();
-n.Ligar();
-n.InstalarAplicativo();
+Console.WriteLine("Smartfone Nokia");
+Smartfone nokia = new Nokia(numero: "123456", Modelo: "Modelo 1", Imei: "1111111111", Memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartfone Iphone");
+Smartfone iphone = new Nokia(numero: "4138", Modelo: "Modelo 2", Imei: "22222222222", Memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
